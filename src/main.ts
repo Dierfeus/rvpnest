6c651c63614f95ced2e84e21b53f31661c4dd50a
Dierfeus <zulin.zulin15@gmail.com>
2026-06-22 16:40:45 +0700

    авторизация сваггер

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,36 @@
-import {NestFactory} from "@nestjs/core"
-import {AppModule} from "./app.module"
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "./pipes/validation.pipe";
-
 
 async function start() {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule);
 
-    const  config = new DocumentBuilder()
-        .setTitle("урок по лалала")
-        .setDescription("Документация по rest api")
+    const config = new DocumentBuilder()
+        .setTitle("Магазин б/у ноутбуков")
+        .setDescription("Документация")
         .setVersion("1.0")
         .addTag("zulin")
+        .addBearerAuth(
+            {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+                name: 'JWT',
+                description: 'Введите JWT токен',
+                in: 'header',
+            },
+            'JWT-auth',
+        )
         .build();
+
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
 
-    app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalPipes(new ValidationPipe());
 
-    await app.listen(PORT, () => console.log(`hehe on PORT: ${PORT}`))
+    await app.listen(PORT, () => console.log(`hehe on PORT: ${PORT}`));
 }
 
-start()
+start();
